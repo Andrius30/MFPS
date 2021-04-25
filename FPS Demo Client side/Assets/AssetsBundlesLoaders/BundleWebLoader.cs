2d6c33250c9b72ad4e07d4118c2752f86edc7eac
Andrius30 <andrius.rom@gmail.com>
2021-04-25 12:20:33 +0300

    Assetbundle loader modified. Now asset bundles loading from web server.

--- FPS Demo Client side/Assets/AssetsBundlesLoaders/BundleWebLoader.cs
+++ FPS Demo Client side/Assets/AssetsBundlesLoaders/BundleWebLoader.cs
@@ -4,30 +4,54 @@ using UnityEngine.Networking;
 
 public class BundleWebLoader : MonoBehaviour
 {
-    public string bundleUrl = "http://localhost/Assetbundles/streaming-enviroment";
+    public string bundleUrl = "http://www.andrejportfolio.lt/Assetbundles/streaming-enviroment";
+
+    #region OLD
+    //IEnumerator Start()
+    //{
+    //    var uwr = UnityWebRequestAssetBundle.GetAssetBundle(bundleUrl);
+    //    while (!uwr.isDone)
+    //    {
+    //        float progress = -uwr.downloadProgress * 100f;
+    //        if (progress >= 99f)
+    //        {
+    //            yield return uwr.SendWebRequest();
+    //            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(uwr);
+    //            var loadAsset = bundle.LoadAllAssets<GameObject>();
+    //            foreach (var item in loadAsset)
+    //            {
+    //                GameObject gm = Instantiate(item);
+    //                SetLayer(gm);
+    //            }
+    //        }
+    //        yield return null;
+    //    }
+    //    Debug.Log($"Asset Bundles Loaded successfully! :15:green;".Interpolate());
+    //} 
+    #endregion
 
     IEnumerator Start()
     {
-        var uwr = UnityWebRequestAssetBundle.GetAssetBundle(bundleUrl);
-        while (!uwr.isDone)
+        UnityWebRequest www = UnityWebRequestAssetBundle.GetAssetBundle(bundleUrl);
+        yield return www.SendWebRequest();
+
+        while (!www.isDone)
         {
-            float progress = -uwr.downloadProgress * 100f;
-            if (progress >= 99f)
-            {
-                yield return uwr.SendWebRequest();
-                AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(uwr);
-                var loadAsset = bundle.LoadAllAssets<GameObject>();
-                foreach (var item in loadAsset)
-                {
-                    GameObject gm = Instantiate(item);
-                    SetLayer(gm);
-                }
-            }
+            Debug.Log("Downloading...");
             yield return null;
         }
-        Debug.Log($"Asset Bundles Loaded successfully! :15:green;".Interpolate());
+        if (www.isDone)
+        {
+            AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(www);
+            GameObject[] gm = bundle.LoadAllAssets<GameObject>();
+            foreach (var item in gm)
+            {
+                Instantiate(item);
+                SetLayer(item);
+            }
+            Debug.Log($"Download complete {bundle}");
+        }
     }
-
     void SetLayer(GameObject gm)
     {
         foreach (var item in gm.GetComponentsInChildren<Transform>())
